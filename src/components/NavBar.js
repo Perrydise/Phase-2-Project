065519a4356cce7a5ed06efb46e16834eac4771e
Perry Jorss <pjorss123@gmail.com>
2022-07-28 17:13:51 -0400

    added style to front page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom"
 const linkStyles = {
     display: "inline-block",
     width: "50px",
-    padding: "12px",
+    padding: "20px 10px",
     margin: "0 6px 6px",
     background: "blue",
     textDecoration: "none",
