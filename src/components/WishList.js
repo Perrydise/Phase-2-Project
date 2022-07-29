1ade8f026c99d4fbe36b8e29df225a4e4779d689
Perry Jorss <pjorss123@gmail.com>
2022-07-29 14:15:30 -0400

    added spacing in css

--- src/components/WishList.js
+++ src/components/WishList.js
@@ -34,8 +34,8 @@ function WishList() {
 
     return (
         <div className="wish-div">
-            <h1>Wish List</h1>
-            <h2>Here you can keep track of projects you got your eye on.
+            <h1 className="wish-header">Wish List</h1>
+            <h2 className="wish-second-header">Here you can keep track of projects you got your eye on.
                 Fill out our form and the project will be added to your list!
             </h2>
             <div className="list-div">
